30982d1468009f2e56c7663e1f2427b0f1e8834a
medhabibdridi <48842413+medhabibdridi@users.noreply.github.com>
2021-03-06 21:53:03 +0100

    LandingPage
    landing page with navigation
    without the bulb image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import LandingPage from './Template/LandingPage';
 
 function App() {
   return (
     <div className="app">
-      
+      <LandingPage/>
     </div>
   );
 }
