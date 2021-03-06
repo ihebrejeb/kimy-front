794992eebccdab59506b4dc51907058845ce5212
medhabibdridi <48842413+medhabibdridi@users.noreply.github.com>
2021-03-06 22:31:58 +0100

    Login + routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
 import LandingPage from './Template/LandingPage';
+import {BrowserRouter as Router , Switch , Route } from "react-router-dom" ; 
+import SignIn from './Components/SignIn';
 
 function App() {
   return (
-    <div className="app">
-      <LandingPage/>
-    </div>
+
+    <Router> 
+    <Switch> 
+           
+    <Route path="/login">
+           <SignIn /> 
+           </Route>
+
+           <Route path="/">
+           <LandingPage /> 
+           </Route>
+
+          
+         
+
+           </Switch>
+    </Router>
   );
 }
 
