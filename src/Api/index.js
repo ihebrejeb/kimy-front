57af2c90dcb285804cbda8e6eb5f4b105d4aaf1c
medhabibdridi <48842413+medhabibdridi@users.noreply.github.com>
2021-03-18 18:50:03 +0100

    Full course crud with design and backend communication
    something is off ( the page requires a refresh after editing or when deleting to see changes )

--- src/Api/index.js
+++ src/Api/index.js
@@ -3,5 +3,8 @@ const url = 'http://localhost:4000/courses' ;
 
  export const fetchCourses =  () =>   axios.get(url);
  export const CreateCourses =  (newCourse) => axios.post(url , newCourse);
+ export const UpdateCourses = (id , updatedCourse) => axios.patch(`${url}/${id}` , updatedCourse);
+ export const deleteCourses = (id ) => axios.delete(`${url}/${id}` );
 
-                                      
+ 
+                  
--- src/Pages/Courses.js
+++ src/Pages/Courses.js
@@ -1,30 +1,30 @@
 import React from 'react'
 import Addcourse from '../features/AppBase/courses/Addcourse'
 import CoursesList from '../features/AppBase/courses/CoursesList'
-
+import styles from './Courses.module.css'
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { GetCourses } from '../features/AppBase/courses/CoursesSlice';
-
+import { useState } from 'react';
+;
 
 function Courses() {
-  
+  const [currentId, setcurrentId] = useState(null) ;
   const dispatch = useDispatch()
   
-
 
   useEffect(() => { 
       dispatch(GetCourses()) ;
 
-  }, [dispatch] ) 
+  }, [currentId, dispatch  ] ) 
   
 
   return (
 
     <div>
-      <div>  <Addcourse/> </div>
+      <div >  <Addcourse currentId={currentId} /> </div>
 
-      <div>  <CoursesList/>  </div>
+      <div className={styles.courses}>  <CoursesList setcurrentId={setcurrentId} />  </div>
           
           
          
