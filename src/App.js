090d8aee9e04b8639bb423d9bf80b3fc0bd37cd0
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-03-20 16:40:11 +0000

    container

--- src/App.js
+++ src/App.js
@@ -8,10 +8,14 @@ import {
   Redirect,
 } from "react-router-dom";
 import SignUp from "./Pages/SignUp";
+import Courses from "./Pages/Courses";
+import Whiteboard from "./Pages/Whiteboard";
+
 import AppBase from "./features/AppBase/AppBase";
 import { ThemeProvider } from "@material-ui/styles";
 import { createMuiTheme } from "@material-ui/core";
 import NoRoute from "./Pages/NoRoute";
+import Container from "./Pages/Container";
 const theme = createMuiTheme({
   palette: {
     primary: {
@@ -33,6 +37,15 @@ function App() {
         </Route>
         <Route exact path="/signup">
           <SignUp />
+        </Route>
+        <Route exact path="/courses">
+          <Courses />
+        </Route>
+        <Route exact path="/whiteboard">
+          <Whiteboard />
+        </Route>
+        <Route exact path="/Container">
+          <Container />
         </Route>
         <Route exact path="/">
           <LandingPage />
