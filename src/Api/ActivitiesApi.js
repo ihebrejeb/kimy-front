b5c6f3f4eb458b1c214217a4cb7e86466a663bdd
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-03-29 23:31:09 +0000

    Update ActivitiesApi.js

--- src/Api/ActivitiesApi.js
+++ src/Api/ActivitiesApi.js
@@ -1,8 +1,9 @@
 import axios from "axios";
-const url = "http://localhost:4000/activities";
+const url = "http://localhost:4000/activity";
 
-export const fetchCourses = () => axios.get(url);
-export const CreateCourses = (newCourse) => axios.post(url, newCourse);
-export const UpdateCourses = (id, updatedCourse) =>
+export const fetchCoursesActivities = () => axios.get(url);
+export const CreateCoursesActivities = (newCourse) =>
+  axios.post(url, newCourse);
+export const UpdateCoursesActivities = (id, updatedCourse) =>
   axios.patch(`${url}/${id}`, updatedCourse);
-export const deleteCourses = (id) => axios.delete(`${url}/${id}`);
+export const deleteCoursesActivities = (id) => axios.delete(`${url}/${id}`);
